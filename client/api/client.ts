404038ccdd0286fbbcd23cd59b416dc7fcf21fe6
PromitSaha <promit.mist@gmail.com>
2026-05-18 23:13:12 -0500

    update the readme file

--- client/api/client.ts
+++ client/api/client.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-  baseURL: "http://10.104.130.57:3000",
+  baseURL: process.env.EXPO_PUBLIC_API_URL,
   headers: {
     "Content-Type": "application/json",
   },
